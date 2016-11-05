9b15a1deda1bcc717134ed54af714774f8f27a53
Andrew He <he.andrew.mail@gmail.com>
2016-11-05 13:20:40 -0400

    Yelp fetching

---
+++ yelp-fetcher.py
@@ -0,0 +1,53 @@
+import io, json
+import math
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+''' Returns Yelp rating of area
+
+Aggregates review counts for top 20 businesses in the region
+
+Params:
+    categories - list of Yelp's: https://www.yelp.com/developers/documentation/v2/all_category_list
+    radius  - in meters
+'''
+def get_local_rating(location, categories, radius=10000, mesh_size=10):
+    # read API keys
+    with io.open('secrets/yelp_secret.json') as cred:
+        creds = json.load(cred)
+        auth = Oauth1Authenticator(**creds)
+        client = Client(auth)
+
+    client = Client(auth)
+
+    businesses = {}
+
+    mesh_dist = float(radius) / mesh_size
+
+    params = {
+            'limit': 20,
+            'offset': 0,
+            'sort': 1,
+            'category_filter': ','.join(categories),
+            'radius_filter': mesh_dist,
+            }
+
+    latitude = location[0]
+    longitude = location[1]
+    meters_to_latitude = 1. / (111000)
+    meters_to_longitude = 1. / (111321 * math.cos(math.radians(latitude)))
+    for i in range(-mesh_size / 2, mesh_size / 2):
+        for j in range(-mesh_size / 2, mesh_size / 2):
+            try:
+                response = client.search_by_coordinates(
+                        latitude + i * mesh_dist * meters_to_latitude,
+                        longitude + j * mesh_dist * meters_to_longitude,
+                        **params)
+                for business in response.businesses:
+                    businesses[business.id] = business
+            except Exception as e:
+                print e.msg
+    return businesses
+
+if __name__ == '__main__':
+    print get_local_rating((39.7467431,-75.5448359),['sushi'])
